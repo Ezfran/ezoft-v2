193ed3230579f65a77964ab139d0b2b8ce63a94a
Ezfran <ezfranquijada.com>
2024-02-04 11:43:09 -0400

    add env

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,8 @@
+import Image from "next/image";
+import Link from "next/link";
 export default function Footer() {
     return (
-      <footer>
+      <footer className="bg-slate-600">
           <section>
             <article>
               <h3>
@@ -25,13 +27,79 @@ export default function Footer() {
                 </li>
               </ul>
             </article>
-            <article></article>
-            <article></article>
-            <article></article>
+            <article>
+              <h3>
+                Enlaces
+              </h3>
+              <ul>
+                <li>
+                  <a href="#">
+                    Términos y condiciones
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Politicas y privacidad
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    Nuestros Servicios
+                  </a>
+                </li>
+              </ul>
+            </article>
+            <article>
+              <h3>
+                ¡Envíanos un Email!
+              </h3>
+              <form action="#">
+                <input type="text" />
+                <button type="button">
+                  Enviar
+                </button>
+              </form>
+            </article>
+            <article>
+              <h3>Trabajos Recientes</h3>
+              <div>
+                <div>
+                  Imagen
+                </div>
+                <div>
+                  <h4>
+                    Quiebratech
+                  </h4>
+                </div>
+              </div>
+              <div>
+                <div>
+                  Imagen
+                </div>
+                <div>
+                  <h4>
+                    Quiebratech
+                  </h4>
+                </div>
+              </div>
+            </article>
           </section>
-          <div>
-
-          </div>
+          <article>
+            <div>
+              <Image
+                alt="logo"
+                src="/logo.png"
+                width={80}
+                height={80}
+              />
+            </div>
+            <ul>
+              <Link href="#">Inicio</Link>
+              <Link href="#">Nosotros</Link>
+              <Link href="#">Trabajo</Link>
+              <Link href="#">Contacto</Link>
+            </ul>
+          </article>
       </footer>
     );
   }
