1a35a09f240951b858c69a6e43135dd70f8e8b45
Ezfran <ezfranquijada.com>
2024-02-06 22:55:14 -0400

    update prices

--- src/app/(Routes)/services/page.js
+++ src/app/(Routes)/services/page.js
@@ -121,7 +121,7 @@ export default function Services() {
                     <p className='w-7/12 text-center'>
                         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam in et rerum molestiae totam!
                     </p>
-                    <a href='#' className='bg-black text-white w-52 py-3 text-center rounded-[4px]'>
+                    <a href='#' className='bg-black text-white w-52 py-3 text-center rounded-[4px] border-2 hover:bg-white hover:border-black hover:text-black transition-all duration-500 border-2 hover:bg-white hover:border-black hover:text-black transition-all duration-500'>
                         Inicia Ahora
                     </a>
                 </header>
@@ -137,7 +137,7 @@ export default function Services() {
             </section>
 
             <section className='px-[5vw] md:px-[15vw] mt-28 mb-16 flex flex-col gap-y-5'>
-                <article className='w-full bg-black text-white lg:h-96 h-auto'>
+                <article className='w-full bg-black text-white lg:h-96 h-auto rounded-[4px]'>
                     <header className='flex flex-col gap-y-4 sm:flex-row justify-center sm:justify-between pb-3 border-b-[1px] border-[rgba(255,255,255,.3)] p-5'>
                         <p className='text-2xl'>
                             Plan Personalizado
@@ -201,14 +201,14 @@ export default function Services() {
                                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
                             </p>
                         </div>
-                        <a href='#' className='bg-white text-black w-36 sm:w-48 flex items-center justify-center h-10 text-center rounded-[4px]'>
+                        <a href='#' className='bg-white text-black w-36 sm:w-48 flex items-center justify-center h-10 text-center rounded-[4px] border-2 hover:bg-black hover:border-white hover:text-white transition-all duration-500'>
                             Inicia Ahora
                         </a>
                     </article>
                 </article>
                 <div className='flex flex-col md:flex-row gap-5 items-start justify-center'>
                     
-                    <article className='border-[1px] border-[rgba(0,0,0,.3)] w-full md:w-[50%]'>
+                    <article className='border-[1px] border-[rgba(0,0,0,.3)] w-full md:w-[50%] rounded-[4px]'>
                         <header className='flex flex-col gap-y-4 sm:flex-row justify-center sm:justify-between pb-3 border-b-[1px] border-[rgba(0,0,0,.3)] p-5'>
                             <p className='text-2xl'>
                                 Plan 1
@@ -249,13 +249,13 @@ export default function Services() {
                                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
                                 </p>
                             </div>
-                            <a href='#' className='bg-black text-white w-36 sm:w-48 flex items-center justify-center h-10 text-center rounded-[4px]'>
+                            <a href='#' className='bg-black text-white w-36 sm:w-48 flex items-center justify-center h-10 text-center rounded-[4px] border-2 hover:bg-white hover:border-black hover:text-black transition-all duration-500'>
                                 Inicia Ahora
                             </a>
                         </article>
                     </article>
 
-                    <article className='border-[1px] border-[rgba(0,0,0,.3)] w-full md:w-[50%]'>
+                    <article className='border-[1px] border-[rgba(0,0,0,.3)] w-full md:w-[50%] rounded-[4px]'>
                         <header className='flex flex-col gap-y-4 sm:flex-row justify-center sm:justify-between pb-3 border-b-[1px] border-[rgba(0,0,0,.3)] p-5'>
                             <p className='text-2xl'>
                                 Plan 1
@@ -296,7 +296,7 @@ export default function Services() {
                                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
                                 </p>
                             </div>
-                            <a href='#' className='bg-black text-white w-36 sm:w-48 flex items-center justify-center h-10 text-center rounded-[4px]'>
+                            <a href='#' className='bg-black text-white w-36 sm:w-48 flex items-center justify-center h-10 text-center rounded-[4px] border-2 hover:bg-white hover:border-black hover:text-black transition-all duration-500'>
                                 Inicia Ahora
                             </a>
                         </article>
@@ -305,13 +305,74 @@ export default function Services() {
                 </div>
             </section>
 
+
+            <section className='px-[5vw] md:px-[15vw] flex flex-col gap-10'>
+                <article className='flex flex-col gap-5 border-b-[1px] border-[rgba(0,0,0,.3)] pb-10'>
+                    <header className='flex gap-y-2 lg:flex-row flex-col items-start justify-between w-full lg:items-center '>
+                        <h3 className='text-2xl flex md:flex-row flex-col items-start md:items-center gap-2.5  border-b-[1px] md:border-b-[0px] border-[rgba(0,0,0,.3)]'>
+                            <span className='inline-block bg-black text-white uppercase text-sm rounded-[4px] px-3 py-0.5 tracking-wider'>
+                                Estándar
+                            </span>
+                            Aplicación Movíl 
+                        </h3>
+                        <p className='uppercase font-medium text-sm tracking-wide'>
+                            Incluye un Descuento para una Web package
+                        </p>
+                    </header>
+                    <div className='flex gap-y-5 md:flex-row flex-col items-start md:items-center w-full justify-between relative'>
+                        <p className='w-full md:w-4/6'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, quasi. Ullam laborum quod minus blanditiis perferendis iusto molestiae soluta distinctio.
+                        </p>
+                        <p className='text-3xl flex items-center gap-2'>
+                            1299.99$
+                        </p>
+                    </div>
+                </article>
+                <article className='flex flex-col gap-5 border-b-[1px] border-[rgba(0,0,0,.3)] pb-10'>
+                    <header className='flex gap-y-2 lg:flex-row flex-col items-start justify-between w-full lg:items-center '>
+                        <h3 className='text-2xl flex md:flex-row flex-col items-start md:items-center gap-2.5  border-b-[1px] md:border-b-[0px] border-[rgba(0,0,0,.3)]'>
+                            <span className='inline-block bg-black text-white uppercase text-sm rounded-[4px] px-3 py-0.5 tracking-wider'>
+                                Completa
+                            </span>
+                            Aplicación Movíl
+                        </h3>
+                        <p className='uppercase font-medium text-sm tracking-wide'>
+                            Incluye un Descuento para una Web package
+                        </p>
+                    </header>
+                    <div className='flex gap-y-5 md:flex-row flex-col items-start md:items-center w-full justify-between relative'>
+                        <p className='w-full md:w-4/6'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, quasi. Ullam laborum quod minus blanditiis perferendis iusto molestiae soluta distinctio.
+                        </p>
+                        <p className='text-3xl '>
+                            2299.99$
+                        </p>
+                    </div>
+                </article>
+
+
+                <article>
+                    <div className='flex gap-y-5 md:flex-row flex-col items-start md:items-center w-full justify-between relative'>
+                        <p className='w-full md:w-4/6'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, quasi. Ullam laborum quod minus blanditiis perferendis iusto molestiae soluta distinctio.
+                        </p>
+                        <div className='md:w-[50%] w-full flex items-start md:items-end justify-start md:justify-end'>
+                            <a href="#" className="text-sm px-3 md:px-5 py-2 border-2 border-[rgba(0,0,0,.6)] text-black md:text-base rounded-[4px] hover:bg-black hover:border-white hover:text-white transition-all duration-500">
+                                Elija el mejor para ti
+                            </a> 
+                        </div>
+                    </div>
+                </article>
+
+            </section>
+
             
             <section className='flex justify-center items-center mt-28 px-[15vw]'>
                 <div className='text-center border-y-2 border-black border-opacity-20 w-full flex md:flex-row flex-col gap-x-14 gap-y-3 justify-center items-center h-40 sm:h-32 md:h-24'>
                     <h3 className='text-2xl font-semibold'>
                         ¿Estás buscando algo más?
                     </h3>
-                    <a href="#" className="px-3 md:px-6 py-2 bg-black text-white text-sm md:text-lg rounded-[4px]">
+                    <a href="#" className="px-3 md:px-6 py-2 bg-black text-white text-sm md:text-lg rounded-[4px] hover:bg-white hover:border-black border-2 hover:text-black transition-all duration-500">
                         ¡Escríbanos!
                     </a> 
                 </div>
